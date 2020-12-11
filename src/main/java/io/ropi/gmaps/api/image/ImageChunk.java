4c13b0c65dd8355fde95e21af4513b6cd124f3be
László Stahorszki <laszlo.stahorszki@ropi.dev>
2020-12-11 19:46:41 +0100

    performance enhancement

--- src/main/java/io/ropi/gmaps/api/image/ImageChunk.java
+++ src/main/java/io/ropi/gmaps/api/image/ImageChunk.java
@@ -6,13 +6,14 @@ import java.awt.Graphics;
 import java.awt.Image;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
-import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
 import io.ropi.gmaps.api.map.Chunk;
 import io.ropi.gmaps.api.parse.Position;
 
+import lombok.Builder;
+import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.ResourceLoader;
@@ -24,7 +25,7 @@ import org.springframework.stereotype.Component;
 public class ImageChunk {
 
     private final ResourceLoader resourceLoader;
-    private final Map<String, Image> imageCache = new ConcurrentHashMap<>();
+    private final Map<CacheKey, Image> imageCache = new ConcurrentHashMap<>();
 
     public ImageChunk(ResourceLoader resourceLoader) {
         this.resourceLoader = resourceLoader;
@@ -32,24 +33,22 @@ public class ImageChunk {
 
     public BufferedImage imageChunk(Chunk chunk, float width, float height) {
         BufferedImage bufferedImage = new BufferedImage(Math.round(width), Math.round(height), BufferedImage.TYPE_INT_ARGB);
+
         int chunkSize = (int)chunk.getTiles().keySet().parallelStream().map(Position::getX).distinct().count();
         float tileSize = (float)width / chunkSize;
+        int size = (int) Math.ceil(tileSize);
 
+        Graphics graphics = bufferedImage.getGraphics();
         chunk.getTiles().entrySet().parallelStream()
-                .map(entry -> Map.entry(entry.getKey(), getColorOfTile(entry.getValue())))
+                .map(entry -> Map.entry(entry.getKey(), getColorOfTile(CacheKey.builder().name(entry.getValue()).size(size).build())))
                 .forEach(entry -> {
-                    Graphics graphics = bufferedImage.getGraphics();
                     int x = Math.round(entry.getKey().getX() * tileSize);
                     int y = Math.round(entry.getKey().getY() * tileSize);
-                    int size = (int) Math.ceil(tileSize);
 
                     graphics.drawImage(entry.getValue(), x, y, size, size, null);
-
-
                 });
 
         if(chunk.getVisibleForces().isEmpty()) {
-            Graphics graphics = bufferedImage.getGraphics();
             graphics.setColor(new Color(0, 0, 0, 0.5f));
             graphics.fillRect(0, 0, (int)width, (int)height);
         }
@@ -58,28 +57,36 @@ public class ImageChunk {
     }
 
     @NonNull
-    public Image getColorOfTile(String name) {
-        if(!imageCache.containsKey(name)) {
+    public Image getColorOfTile(CacheKey key) {
+        if(!imageCache.containsKey(key)) {
             synchronized (imageCache) {
-                if (!imageCache.containsKey(name)) {
-                    Resource resource = resourceLoader.getResource("classpath:assets/terrain/" + name + ".png");
+                if (!imageCache.containsKey(key)) {
+                    Resource resource = resourceLoader.getResource("classpath:assets/terrain/" + key.getName() + ".png");
 
                     Image image;
                     try {
-                        image = ImageIO.read(resource.getInputStream()).getSubimage(0, 0, 32, 32);
+                        image = ImageIO.read(resource.getInputStream()).getSubimage(0, 0, 32, 32)
+                                .getScaledInstance(key.getSize(), key.getSize(), Image.SCALE_FAST);
                     } catch (IOException e) {
-                        log.error("Could not find asset with name: {}", name);
-                        image = new BufferedImage(256, 256, BufferedImage.TYPE_INT_RGB);
+                        log.error("Could not find asset with name: {}", key.getName());
+                        image = new BufferedImage(key.getSize(), key.getSize(), BufferedImage.TYPE_INT_RGB);
                     }
 
-                    imageCache.put(name, image);
+                    imageCache.put(key, image);
                     return image;
                 } else {
-                    return imageCache.get(name);
+                    return imageCache.get(key);
                 }
             }
         }
 
-        return imageCache.get(name);
+        return imageCache.get(key);
+    }
+
+    @Data
+    @Builder
+    static class CacheKey {
+        private String name;
+        private int size;
     }
 }
